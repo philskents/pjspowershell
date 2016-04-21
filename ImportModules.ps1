adaaee51976136774ce5665b972234759e4f3a38
Phillip Skentelbery <pskentelbery@aplicor.com>
2016-04-21 14:59:06 -0400

    Added ImportModules.ps1

---
+++ ImportModules.ps1
@@ -0,0 +1,19 @@
+﻿#Specify the modules required to execute a script
+
+#Required modules (as array)
+
+$requiredModules = "ActiveDirectory", "SQLPS"
+
+# Iterate through the array, importing each module if available
+
+foreach ($module in $requiredModules){
+    if ( (Get-Module -Name $module -ErrorAction silentlycontinue) -eq $null ) {
+        if ( (Get-Module -ListAvailable -Name $module -ErrorAction SilentlyContinue) -eq $null) {
+            Write-Error "You must install" $module " Module to use this script!"
+        } 
+        else {
+            Write-Host "Importing " $module
+            Import-Module $module -ErrorAction Stop
+        }
+    }
+}
