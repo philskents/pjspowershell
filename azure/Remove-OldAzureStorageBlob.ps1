18b020b631aaa8ba6792d950801bc34b2061238f
Phil Skentelbery <pjs@aplicor.com>
2016-08-17 14:34:05 -0400

    Added help block and additional arguments to Remove-OldAzureStorageBlob

--- azure/Remove-OldAzureStorageBlob.ps1
+++ azure/Remove-OldAzureStorageBlob.ps1
@@ -1,4 +1,19 @@
-﻿[CmdletBinding()]
+﻿<#
+.Synopsis
+   This script will delete all blobs from the specified container over a certain age.
+.DESCRIPTION
+   When given the necessary storage account information this script will find any blobs over the given age in days and
+   remove them. If the job fails it will send an email to the specified recipient.
+
+   This script is designed to be executed as a scheduled task.
+.EXAMPLE
+   .\Remove-OldAzureStorageBlob.ps1 -storageAccountName foo -storageAccountKey bar -containerName backups -age 30 -errorSMTPServer mymail.domain.com -errorSourceAddress alerts@domain.com -errorDestAddress me@domain.com
+.NOTES
+   Author:    Phil Skents (phil@strangeadventures.in)
+   URL:       https://strangeadventures.in
+#>
+
+[CmdletBinding()]
 Param(
 [Parameter (Mandatory=$true,Position=1)]
 [string]$storageAccountName,
@@ -13,7 +28,13 @@ Param(
 [string]$age,
 
 [Parameter (Mandatory=$true,Position=5)]
-[string]$errorSMTPServer
+[string]$errorSMTPServer,
+
+[Parameter (Mandatory=$true,Position=9)]
+[string]$errorSourceAddress,
+
+[Parameter (Mandatory=$true,Position=5)]
+[string]$errorDestAddress
 )
 
 $context = New-AzureStorageContext -StorageAccountName $storageAccountName -StorageAccountKey $storageAccountKey
@@ -29,5 +50,5 @@ Catch{
     $sub = 'Job failed on '+$hostname
     $err = $_.Exception.Message
     $msg = 'Scheduled job Remove-OldAzureStorageBlob failed on '+$hostname+' with error '+$err
-    Send-MailMessage -From alerts@aplicor.com -To opshelpdesk@aplicor.com -Subject $sub -Body $msg -SmtpServer $errorSMTPServer
+    Send-MailMessage -From $errorSourceAddress -To $errorDestAddress -Subject $sub -Body $msg -SmtpServer $errorSMTPServer
 }
