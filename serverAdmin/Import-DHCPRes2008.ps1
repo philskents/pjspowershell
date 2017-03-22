38095a85416035d9943a24173651b4026c94221e
Phillip Skentelbery <Phil.Skentelbery@heat.com>
2017-03-22 12:57:27 -0400

    Added comments to Import-DHCPRes2008.ps1

--- serverAdmin/Import-DHCPRes2008.ps1
+++ serverAdmin/Import-DHCPRes2008.ps1
@@ -1,14 +1,18 @@
 [CmdletBinding()]
 Param(
+    #Get path to CSV 
     [Parameter (Mandatory=$true,Position=1)]
     [string]$pathToCsv,
 
+    #Get DHCP Server ID
     [Parameter (Mandatory=$true,Position=2)]
     [string]$dhcpServer
 )
 
+#Import CSV file
 $Reservations = Import-Csv -Path $pathToCsv
 
+#Standardize MAC format to xx:xx:xx:xx:xx:xx
 foreach ($Reservation in $Reservations){
     $dirtyMac = $Reservation.Mac
     $dirtyMac = $dirtyMac -replace '[^a-zA-Z0-9]',''
@@ -18,19 +22,23 @@ foreach ($Reservation in $Reservations){
     $dirtyMac = $dirtyMac.insert(11,":")
     $cleanMac = $dirtyMac.insert(14,":")
 
+    #Check to see if IP address is valid
     if ([bool]($Reservation.IPAddress -as [ipaddress]) -eq $false){
         $message = "The IP Address "+$Reservation.IPAddress+" is invalid, skipping"
         Write-Host $message
     }
+    #Check MAC to see if it is valid 
     elseif ($cleanMac -notmatch '^([0-9A-Fa-f]{2}[:-]){5}([0-9A-Fa-f]{2})$'){
         $message = "The MAC address for "+$Reservation.IPAddress+" is invalid, skipping"
         Write-Host $message
     }
     else {
         try {
+            #Remove delimitters from mac address (required for netsh command)
             $mac = $cleanMac -replace '[^a-zA-Z0-9]',''
+            #Build netsh command 
             $cmd = 'netsh Dhcp Server '+$dhcpServer+' Scope '+$Reservation.ScopeId+' Add reservedip '+$Reservation.IPAddress+' '+$mac+' "'+$Reservation.Name+'" "'+$Reservation.Description+'" BOTH'
-            #Write-Host $cmd
+            #Execute netsh command 
             Invoke-Expression $cmd | Out-Null
             $message = "Reservation for "+$Reservation.IPAddress+" Added to scope"
             Write-Host $message
